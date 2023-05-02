633761588b96acaa1cce58af6a355dfcb06bde64
Kuldeep-Kumar-150 <swamikuldeep654@gmail.com>
2023-05-02 10:05:27 +0530

    first commit

---
+++ src/components/MyNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyNav = () => {
+  return (
+    <section>
+      <p className='ff_highspeed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae illo omnis quam.
+      </p>
+    </section>
+  )
+}
+
+export default MyNav
