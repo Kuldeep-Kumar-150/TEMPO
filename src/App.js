e5c0d74246e86ca5af81983f7a5c0b1894146f22
Kuldeep-Kumar-150 <swamikuldeep654@gmail.com>
2023-05-02 10:10:33 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import MyNav from './components/MyNav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <div>
-      <MyNav/>
-   </div>
+      <MyNav />
+    </div>
   );
 }
 
--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const MyNav = () => {
   return (
     <section>
-      <p className='ff_highspeed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae illo omnis quam.
-      </p>
+
     </section>
   )
 }
