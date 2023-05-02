d655b790a23e44921173efbdfe8e5a091847c96b
Kuldeep-Kumar-150 <swamikuldeep654@gmail.com>
2023-05-02 16:51:08 +0530

    lastchange

--- src/components/OurNft.jsx
+++ src/components/OurNft.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Slider from 'react-slick'
 import { Col, Container, Row } from 'react-bootstrap'
 import sliderImg1 from '../assets/img/webp/man_img_1.webp'
 import sliderImg2 from '../assets/img/webp/man_img_2.webp'
 import sliderImg3 from '../assets/img/webp/man_img_3.webp'
+import nextBtn from '../assets/img/svg/next_btn.svg'
+import prevBtn from '../assets/img/svg/prev_btn.svg'
 const OurNft = () => {
+    const first = useRef()
     const settings = {
         dots: false,
         arrows: false,
         infinite: true,
         speed: 500,
-        // autoplay: true,
+        autoplay: true,
         autoplaySpeed: 1000,
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -18,12 +21,14 @@ const OurNft = () => {
         centerPadding: 0,
         responsive: [
             {
-                breakpoint:992,
+                breakpoint: 992,
                 settings: {
                     arrows: false,
                     centerMode: true,
                     slidesToShow: 2,
-                    slidesToScroll:1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 800,
                 }
             },
             {
@@ -31,7 +36,7 @@ const OurNft = () => {
                 settings: {
                     arrows: false,
                     centerMode: true,
-                    slidesToShow: 1
+                    slidesToShow: 1,
                 }
             },
             {
@@ -45,11 +50,14 @@ const OurNft = () => {
         ]
     };
     return (
-        <section className='py-5 mt-5 mt-sm-0 py-sm-3'>
+        <section className='py-5 mt-5 mt-sm-0 py-sm-3 position-relative'>
+            <img onClick={() => first.current.slickPrev()} className='crusor_pointer prev_btn d-none d-md-flex' width={50} height={50} src={prevBtn} alt="prevBtn" />
+            <img onClick={() => first.current.slickNext()} className='crusor_pointer next_btn d-none d-md-flex' width={50} height={50} src={nextBtn} alt="nextBtn" />
             <Container className='overflow-hidden'>
                 <h5 className='text-white text-center fs_md fw-normal ff_highspeed pb-4'>our nft</h5>
                 <Row className=' align-items-end py-5 '>
-                    <Slider {...settings} className='py-5 h_500 my-5'>
+
+                    <Slider {...settings} ref={first} className='py-5 h_500 my-5 '>
                         <Col sm={4}>
                             <div className="our_nft_cards position-relative mx-3">
                                 <img className='position-absolute our_nft_img_1' src={sliderImg1} alt="sliderImg1" />
@@ -71,6 +79,7 @@ const OurNft = () => {
                             </div>
                         </Col>
                     </Slider>
+
                 </Row>
             </Container>
         </section>
--- src/components/WatchVideo.jsx
+++ src/components/WatchVideo.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import comingSoonImg from '../assets/img/webp/coming-soon-img.webp'
 import videoIcon from '../assets/img/svg/video_icon.svg'
+import leftPosition from "../assets/img/svg/leftPosition.svg";
+import rightPosition from "../assets/img/svg/rightPosition.svg";
 const WatchVideo = () => {
     return (
-        <section className='py-sm-5 my-sm-5'>
+        <section className='py-sm-5 my-sm-5 z_3 position-relative'>
+            <img
+                className="position-absolute top-0 start-0 w_6 zn_1"
+                src={leftPosition}
+                alt="leftPosition"
+            />
+            <img
+                className="position-absolute bottom-0 end-0 w_6 zn_1"
+                src={rightPosition}
+                alt="rightPosition"
+            />
+           
+            <div className="blueBoxPosition position-absolute top-0 start-0 zn_1"></div>
+            <div className="blueBoxPosition position-absolute bottom-0 end-0   zn_1"></div>
             <Container className='py-sm-5'>
+
                 <h6 className='fs_md text-white ff_highspeed fw-normal text-center'>watch video</h6>
                 <div className="position-relative mt-4">
                     <img className='w-100' src={comingSoonImg} alt="comingSoonImg" />
